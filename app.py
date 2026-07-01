3109d127dbdacdda090fea321b3600ba8dd60484
vishaldayalu12345-web <vishaldayalu12345@gmail.com>
2026-07-01 23:55:23 +0530

    Day 2: Added form handling and Flask data rendering

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+
+    if request.method == 'POST':
+
+        text = request.form['text']
+        source_language = request.form['source_language']
+        target_language = request.form['target_language']
+
+        return render_template(
+            'index.html',
+            text=text,
+            source_language=source_language,
+            target_language=target_language
+        )
+
     return render_template('index.html')
 
 if __name__ == '__main__':
